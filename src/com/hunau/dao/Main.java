fea4a1b4da860664bf4173ae9d737cc003d804b8
cxhhh <2213087097@qq.com>
2019-01-14 18:22:48 +0800

    new branches

--- src/com/hunau/dao/Main.java
+++ src/com/hunau/dao/Main.java
@@ -3,7 +3,7 @@ package com.hunau.dao;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello，thank you for your contribution");
+	    System.out.println("hello，thank you  your contribution");
     }
 
 }
