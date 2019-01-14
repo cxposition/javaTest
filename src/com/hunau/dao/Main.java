b3df35461c8fd669f9f92e35342ffb05b417419c
cxhhh <2213087097@qq.com>
2019-01-14 17:26:57 +0800

    the third changes

--- src/com/hunau/dao/Main.java
+++ src/com/hunau/dao/Main.java
@@ -3,6 +3,7 @@ package com.hunau.dao;
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("hello，myGold");
+	    System.out.println("hello，thank you for your contribution");
     }
+
 }
